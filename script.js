358a205dc253985e6daf7ac20b1b6f3c8bc80499
Melyssa <m.g.mayhem@gmail.com>
2021-07-03 10:37:37 -0400

    Update all files

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ window.onload = () => defaultGrid();
 
 function defaultGrid() {
     //notes in notebook pg 86
+
+    let grid = 16;
+    let num = grid * grid;
+
+    for (i = 0; i < num; i++) {
+        /*let box = document.getElementById("blueprint");
+        let copy = box.cloneNode(true);
+        let container = document.getElementById("container");
+
+        container.appendChild(copy);*/
+
+        let box = document.createElement("div");
+        let container = document.getElementById("container");
+        container.appendChild(box);
+    }
 }
